e8104c3045a874c4ea2f0eba6168606ba7579e60
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-10-24 16:36:46 -0200

    Finish basic waveform plot

--- waveform.go
+++ waveform.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 )
 
 func abortonerr(err error, op string) {
@@ -26,6 +27,7 @@ func loadAudio(audiofile string) []int16 {
 	data, err := ioutil.ReadAll(f)
 	abortonerr(err, "opening audio file")
 
+	// TODO: parse wav header instead of just skip it
 	data = data[wavHeaderSize:]
 	for i := 0; i < len(data)-1; i += 2 {
 		sample := binary.LittleEndian.Uint16(data[i : i+2])
@@ -35,8 +37,33 @@ func loadAudio(audiofile string) []int16 {
 	return audio
 }
 
+func plotAudio(imgfile string, audio []int16, samplerate int) {
+	f, err := ioutil.TempFile("", "waveform")
+	abortonerr(err, "creating tmp file to generate waveform")
+	defer f.Close()
+
+	fsamplerate := float32(samplerate)
+
+	for i, sample := range audio {
+		timestamp := float32(i) / fsamplerate
+		f.WriteString(fmt.Sprintf("%f %d\n", timestamp, sample))
+	}
+
+	fmt.Printf("tmp gnuplot file: [%s]\n", f.Name())
+	gnuplotargs := fmt.Sprintf("set terminal svg; set output '%s'; plot '%s' with lines", imgfile, f.Name())
+	cmd := exec.Command("gnuplot", "-e", gnuplotargs)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	err = cmd.Run()
+	abortonerr(err, "running gnuplot")
+
+	//TODO: delete tmp file
+}
+
 func main() {
+	const samplerate = 8000
 	var audiofile string
+
 	flag.StringVar(&audiofile, "audio", "", "path to audio file (only WAV PCM 8000Hz LE supported)")
 
 	flag.Parse()
@@ -47,5 +74,7 @@ func main() {
 	}
 
 	audio := loadAudio(audiofile)
-	fmt.Println(audio)
+	plotAudio(audiofile+".waveform.svg", audio, samplerate)
+
+	fmt.Println("done")
 }
